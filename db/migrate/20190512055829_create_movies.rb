e0741e4337b9087d4829f459783507a201c56cb9
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-05-12 06:02:50 +0000

    Automatically backed up by Learn

--- db/migrate/20190512055829_create_movies.rb
+++ db/migrate/20190512055829_create_movies.rb
@@ -3,6 +3,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     add_column :movies do |m|
       m.string :title 
       m.integer :release_date 
+      m.string :director 
     end 
   end
 end
