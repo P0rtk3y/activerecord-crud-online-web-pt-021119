8701de22e19e9b92fcef420c13c576019d42100a
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-05-12 06:33:57 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -92,7 +92,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   destroyed_movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
-  destroyed_movie.remove
+  destroyed_movie.destroy
   __
 end
 
